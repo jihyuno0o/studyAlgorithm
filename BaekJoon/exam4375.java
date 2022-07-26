efdd9da6a0b71f2698a85c4ec2669f7a3e96d502
jihyuno0o <79901413+jihyuno0o@users.noreply.github.com>
2022-07-26 15:47:06 +0900

    0726

---
+++ BaekJoon/exam4375.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class exam4375 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String input;
+		while ((input = br.readLine()) != null) {
+			int target = Integer.parseInt(input);
+			if (target == 1) {
+				System.out.println(1);
+				continue;
+			}
+
+			int n = 1;
+			int cnt = 1;
+			while (true) {
+				n = (int) (n * 10 + 1) % target;
+				cnt++;
+				if (n % target == 0) {
+					System.out.println(cnt);
+					break;
+				}
+			}
+		}
+	}
+}
