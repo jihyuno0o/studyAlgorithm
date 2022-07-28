b52f9651c25553691782309d481a95840fecf20e
jihyuno0o <79901413+jihyuno0o@users.noreply.github.com>
2022-07-28 16:50:45 +0900

    0728

---
+++ BaekJoon/exam2798.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class exam2798 {
+	public static void main(String[] agrs) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int M = sc.nextInt();
+		int[] cards = new int[N];
+		for (int i = 0; i < N; i++) {
+			cards[i] = sc.nextInt();
+		}
+		Arrays.sort(cards);
+
+		int diff = 3000000;
+		int ans = 0;
+		for (int i = 0; i < N; i++) {
+			for (int j = i + 1; j < N; j++) {
+				for (int k = j + 1; k < N; k++) {
+					int temp = cards[i] + cards[j] + cards[k];
+					if (temp > M)
+						break;
+					if (diff > (M - temp)) {
+						diff = M - temp;
+						ans = temp;
+					}
+				}
+			}
+		}
+
+		System.out.println(ans);
+	}
+}
