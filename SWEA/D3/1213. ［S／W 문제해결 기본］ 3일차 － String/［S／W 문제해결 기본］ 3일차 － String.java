7c625386fe0a03c2c8267126d29f7ed81d08db57
jihyuno0o <79901413+jihyuno0o@users.noreply.github.com>
2022-08-12 14:16:52 +0900

    [D3] Title: [S/W 문제해결 기본] 3일차 - String, Time: 105 ms, Memory: 18,956 KB -BaekjoonHub

--- SWEA/D3/1213. ［S／W 문제해결 기본］ 3일차 － String/［S／W 문제해결 기본］ 3일차 － String.java
+++ SWEA/D3/1213. ［S／W 문제해결 기본］ 3일차 － String/［S／W 문제해결 기본］ 3일차 － String.java
@@ -7,18 +7,18 @@ class Solution {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		for(int t=1; t<=10; t++) {
-			br.readLine();
-			String find = br.readLine();
-			char[] inputString = br.readLine().toCharArray();
+			br.readLine(); // 테스트케이스 번호 그냥 넘기기
+			String find = br.readLine(); // 찾을 문자열
+			char[] inputString = br.readLine().toCharArray(); // 문장
 			
-			int ans = 0;
-			for(int i=0; i<inputString.length-find.length()+1; i++) {
-				if(inputString[i] == find.charAt(0)) {
-					int same = 1;
-					for (int j=1; j<find.length(); j++) {
-						if (inputString[i+j] != find.charAt(j)) {
-							same = 0; 
-							break;
+			int ans = 0; // 찾은 문자열 개수
+			for(int i=0; i<inputString.length-find.length()+1; i++) { // 0부터 총문자열-문자개수 까지 탐색
+				if(inputString[i] == find.charAt(0)) { // 첫글자가 같으면
+					int same = 1; 
+					for (int j=1; j<find.length(); j++) { // 그다음 문자열까지 같은지 체크
+						if (inputString[i+j] != find.charAt(j)) { // 다르면
+							same = 0; // 같지않음
+							break; // 나가기
 						}
 					}
 					ans += same;
