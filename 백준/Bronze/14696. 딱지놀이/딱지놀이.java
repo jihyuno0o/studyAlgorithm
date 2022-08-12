cfb28836ee8ea39fd1785a7daf817e2bf8552fe1
jihyuno0o <79901413+jihyuno0o@users.noreply.github.com>
2022-08-12 09:53:39 +0900

    [Bronze I] Title: 딱지놀이, Time: 1220 ms, Memory: 188260 KB -BaekjoonHub

---
+++ 백준/Bronze/14696. 딱지놀이/딱지놀이.java
@@ -0,0 +1,42 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+
+		for (int t = 0; t < n; t++) {
+			int aCnt = sc.nextInt();
+			int[] aCards = new int[aCnt]; // a의 카드 저장
+			for (int i = 0; i < aCnt; i++) {
+				aCards[i] = sc.nextInt();
+			}
+			int bCnt = sc.nextInt();
+			int[] bCards = new int[bCnt]; // b의 카드 저장
+			for (int i = 0; i < bCnt; i++) {
+				bCards[i] = sc.nextInt();
+			}
+			
+			Arrays.sort(aCards);
+			Arrays.sort(bCards);
+
+			String winner = "D";
+			while (aCnt > 0 && bCnt > 0) { //둘다 카드가 남았을 때
+				if (aCards[--aCnt] > bCards[--bCnt]) { // 큰수부터 비교
+					winner = "A";
+					break;
+				} else if (aCards[aCnt] < bCards[bCnt]) {
+					winner = "B";
+					break;
+				}
+
+				if (aCnt == 0 ^ bCnt == 0) { // 둘 중 하나가 0일 때
+					winner = (aCnt > bCnt) ? "A" : "B";
+				}
+			}
+			
+			System.out.println(winner);
+		}
+	}
+}
