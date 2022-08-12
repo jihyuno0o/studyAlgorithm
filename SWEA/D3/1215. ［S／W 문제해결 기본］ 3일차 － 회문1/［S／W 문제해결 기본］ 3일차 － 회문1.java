82daab02ee1e7e854b964ad86ac0cef90eb821fa
jihyuno0o <79901413+jihyuno0o@users.noreply.github.com>
2022-08-12 11:51:47 +0900

    [D3] Title: [S/W 문제해결 기본] 3일차 - 회문1, Time: 107 ms, Memory: 19,284 KB -BaekjoonHub

--- SWEA/D3/1215. ［S／W 문제해결 기본］ 3일차 － 회문1/［S／W 문제해결 기본］ 3일차 － 회문1.java
+++ SWEA/D3/1215. ［S／W 문제해결 기본］ 3일차 － 회문1/［S／W 문제해결 기본］ 3일차 － 회문1.java
@@ -3,6 +3,8 @@ import java.io.InputStreamReader;
 
 // [S/W 문제해결 기본] 3일차 - 회문1
 class Solution {
+	static char[][] map = new char[8][8]; // 8*8 배열 생성
+	
 	public static boolean palindrome(StringBuffer sb) {
 		String str1 = sb.toString();
 		String str2 = sb.reverse().toString();
@@ -11,45 +13,51 @@ class Solution {
 			return true;
 		return false;
 	}
+	
+	public static int findPalindrome(char[][] arr, int len) {
+		int ans = 0;
+		// 가로줄 회문 찾기
+		for (int i = 0; i < 8; i++) { // 행 우선 순회
+			StringBuffer rowSb = new StringBuffer(); // 가로줄 찾을 스트링버퍼
+			for (int j = 0; j < 8; j++) {
+				rowSb.append(map[i][j]); // 스트링버퍼에 더하기
+				if (rowSb.length() > len) {
+					rowSb.delete(0, 1); // len보다 길면 맨앞 문자 빼기
+				}
+				if (rowSb.length() == len && palindrome(rowSb)) { // 회문인지 확인
+					ans++;
+				}
+			}
+		}
+		// 세로줄 회문 찾기
+		for (int j = 0; j < 8; j++) { // 열 우선 순회
+			StringBuffer colSb = new StringBuffer(); // 세로줄 찾을 스트링버퍼
+			for (int i = 0; i < 8; i++) {
+				colSb.append(map[i][j]); // 스트링버퍼에 더하기
+				if (colSb.length() > len) {
+					colSb.delete(0, 1); // len보다 길면 맨앞 문자 빼기
+				}
+				if (colSb.length() == len && palindrome(colSb)) { // 회문인지 확인
+					ans++;
+				}
+
+			}
+		}
+		return ans;
+	}
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		for (int t = 1; t <= 10; t++) {
-			Character[][] map = new Character[8][8]; // 8*8 배열 생성
+			
 			int ans = 0; // 찾은 회문의 개수
 			int len = Integer.parseInt(br.readLine()); // 찾아야하는 회문의 길이
-			// 입력받으면서 가로줄 회문 찾기
-			for (int i = 0; i < 8; i++) { // 행 우선 순회
-				StringBuffer rowSb = new StringBuffer(); // 가로줄 찾을 스트링버퍼
-				String str = br.readLine(); // 한줄씩 읽기
-				for (int j = 0; j < 8; j++) {
-					map[i][j] = str.charAt(j);
-					rowSb.append(map[i][j]); // 스트링버퍼에 더하기
-					if (rowSb.length() > len) {
-						rowSb.delete(0, 1); // len보다 길면 맨앞 문자 빼기
-					}
-					if (rowSb.length() == len && palindrome(rowSb)) { // 회문인지 확인
-						ans++;
-					}
-				}
+			// 입력
+			for (int i = 0; i < 8; i++) {
+				map[i] = br.readLine().toCharArray(); 
 			}
-			// 세로줄 회문 찾기
-			for (int j = 0; j < 8; j++) { // 열 우선 순회
-				StringBuffer colSb = new StringBuffer(); // 세로줄 찾을 스트링버퍼
-				for (int i = 0; i < 8; i++) {
-					colSb.append(map[i][j]); // 스트링버퍼에 더하기
-					if (colSb.length() > len) {
-						colSb.delete(0, 1); // len보다 길면 맨앞 문자 빼기
-					}
-					if (colSb.length() == len && palindrome(colSb)) { // 회문인지 확인
-						ans++;
-					}
-
-				}
-			}
-
-			System.out.printf("#%d %d\n", t, ans);
+			System.out.printf("#%d %d\n", t, findPalindrome(map, len));
 		}
 	}
 }
