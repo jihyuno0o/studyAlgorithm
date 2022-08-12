9abb76d558222751f721123c71c9c5230b6f4186
jihyuno0o <79901413+jihyuno0o@users.noreply.github.com>
2022-08-12 10:15:57 +0900

    [D3] Title: 농작물 수확하기, Time: 181 ms, Memory: 28,800 KB -BaekjoonHub

---
+++ SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Solution {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		for (int t=1; t<=T; t++) {
+			int n = sc.nextInt();
+			int[][] map = new int[n][n];
+			for (int i=0; i<n; i++) {
+				String str = sc.next();
+				int[] line = new int[n];
+				for (int j=0; j<n; j++) {
+					line[j] = str.charAt(j)-'0';
+				}
+				map[i] = line;
+			}
+			
+			int ans = 0;
+			int start = n/2;
+			int end = n/2;
+			for (int i=0; i<n; i++) {
+				for (int j=start; j<=end; j++) {
+					ans += map[i][j];
+				}
+				if (i < n/2) {
+					start -= 1;
+					end += 1;
+				} else {
+					start += 1;
+					end -= 1;
+				}
+			}
+			System.out.printf("#%d %d\n", t, ans);
+		}
+	}
+}
