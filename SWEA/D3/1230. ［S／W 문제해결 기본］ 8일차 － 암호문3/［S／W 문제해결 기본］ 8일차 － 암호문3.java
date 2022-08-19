337975ff03aaf0503f13d2ff6e288de5cb84e3e6
jihyuno0o <79901413+jihyuno0o@users.noreply.github.com>
2022-08-19 11:39:52 +0900

    [D3] Title: [S/W 문제해결 기본] 8일차 - 암호문3, Time: 182 ms, Memory: 30,744 KB -BaekjoonHub

--- SWEA/D3/1230. ［S／W 문제해결 기본］ 8일차 － 암호문3/［S／W 문제해결 기본］ 8일차 － 암호문3.java
+++ SWEA/D3/1230. ［S／W 문제해결 기본］ 8일차 － 암호문3/［S／W 문제해결 기본］ 8일차 － 암호문3.java
@@ -7,8 +7,9 @@ class Solution {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer stk;
-		LinkedList<String> password = new LinkedList<>();
+		LinkedList<String> password;
 		for (int t = 1; t <= 10; t++) {
+             password = new LinkedList<>();
 			br.readLine(); // 원본 암호문의 길이
 			stk = new StringTokenizer(br.readLine(), " ");// 원본 암호문
 			while (stk.hasMoreTokens()) {
