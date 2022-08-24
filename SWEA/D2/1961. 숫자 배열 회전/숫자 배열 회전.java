7ae2a7618b0afb2d6095557b8f2be63fe5222b58
jihyuno0o <79901413+jihyuno0o@users.noreply.github.com>
2022-08-24 17:27:39 +0900

    [D2] Title: 숫자 배열 회전, Time: 108 ms, Memory: 18,400 KB -BaekjoonHub

--- SWEA/D2/1961. 숫자 배열 회전/숫자 배열 회전.java
+++ SWEA/D2/1961. 숫자 배열 회전/숫자 배열 회전.java
@@ -1,59 +1,44 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Solution {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 	public static void main(String[] args) throws Exception {
-		int t = Integer.parseInt(br.readLine().trim());
-		for(int i=1; i<=t; i++) {
+		int T = Integer.parseInt(br.readLine().trim());
+		for (int t = 1; t <= T; t++) {
 			int n = Integer.parseInt(br.readLine().trim());
-			String[][] out = new String[n][3];
+			String[][] output = new String[n][3];
 
-			String[][] arr = new String[n][n];
+			String[][] nums = new String[n][n];
 
 			// input 받기
-			for (int j=0; j<n; j++) {
+			for (int i = 0; i < n; i++) {
 				String[] input = br.readLine().split(" ");
-				for (int k=0; k<n; k++) {
-					arr[j][k] = input[k];
+				for (int j = 0; j < n; j++) {
+					nums[i][j] = input[j];
 				}
-			}
-			
-			//90도 돌리기
-			for (int cnt=0; cnt<3; cnt++) {
-				String[][] after = new String[n][n];			
-				for (int j=n; j>0; j--) {
-					for (int k=0; k<n; k++) {
-						after[k][n-j] = arr[j-1][k];
-					}
-				}
-				for( int j=0; j<n; j++)
-					out[j][cnt] = String.join("", after[j]);
-				
-				arr = after;
-			}
-			
-			System.out.printf("#%d\n",i);
-			for( int j=0; j<n; j++) {
-				System.out.println(String.join(" ", out[j]));
 			}
 
+			// 90도 돌리기
+			for (int cnt = 0; cnt < 3; cnt++) {
+				String[][] after = new String[n][n];
+				for (int i = n; i > 0; i--) {
+					for (int j = 0; j < n; j++) {
+						after[j][n - i] = nums[i - 1][j];
+					}
+				}
+				for (int j = 0; j < n; j++)
+					output[j][cnt] = String.join("", after[j]);
+
+				nums = after;
+			}
+
+			System.out.printf("#%d\n", t);
+			for (int i = 0; i < n; i++) {
+				System.out.println(String.join(" ", output[i]));
+			}
 
 		}
 	}
-
-//	public static String[][] turn(String[][] arr){
-//		String[][] before = arr;
-//		int n = arr.length;
-//		String[][] after = new String[n][n];
-//
-//		for (int i=n; i>0; i--) {
-//			for (int j=0; j<n; j++) {
-//				after[j][n-i] =  before[i][j];
-//			}
-//		}
-//
-//		return after;
-//	}
 }
