d4e756d2e77bf308c1b02a611fcf86625badedd5
jihyuno0o <79901413+jihyuno0o@users.noreply.github.com>
2022-09-07 19:15:10 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 144 ms, Memory: 17356 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -3,16 +3,6 @@ import java.util.Queue;
 import java.util.Scanner;
 
 // 숨바꼭질 3
-class Loc {
-	int pos;
-	int time;
-
-	public Loc(int pos, int time) {
-		this.pos = pos;
-		this.time = time;
-	}
-}
-
 public class Main {
 	static int[] check = new int[100001];
 	static int N, K;
@@ -26,32 +16,32 @@ public class Main {
 			System.out.println(0); // 입력이 같을 때
 		else {
 			bfs();
+			System.out.println(check[K]-1);
 		}
 	}
 
 	public static void bfs() {
-		Queue<Loc> que = new LinkedList<>();
-		que.add(new Loc(N, 0));
-
+		Queue<Integer> que = new LinkedList<>();
+		que.add(N);
+        check[N] = 1;
 		while (!que.isEmpty()) {
-			Loc X = que.poll();
-			if (X.pos == K) {
-				System.out.println(X.time);
+			int X = que.poll();
+			if (X == K)
 				break;
+            
+            if (X * 2 < check.length && check[X * 2] == 0) { // X*2
+				que.add(X * 2);
+				check[X * 2] = check[X]; // 0초후
 			}
-
-			if (X.pos * 2 < check.length && check[X.pos * 2] == 0) { // X*2
-				que.add(new Loc(X.pos * 2, X.time));
-				check[X.pos * 2]++; // 0초후
+			if (X > 0 && check[X - 1] == 0) { // X-1
+				que.add(X - 1);
+				check[X - 1] = check[X] + 1; // 1초후
 			}
-			if (X.pos > 0 && check[X.pos - 1] == 0) { // X-1
-				que.add(new Loc(X.pos - 1, X.time + 1));
-				check[X.pos - 1]++; // 1초후
+			if (X + 1 < check.length && check[X + 1] == 0) { // X+1
+				que.add(X + 1);
+				check[X + 1] = check[X] + 1; // 1초후
 			}
-			if (X.pos + 1 < check.length && check[X.pos + 1] == 0) { // X+1
-				que.add(new Loc(X.pos + 1, X.time + 1));
-				check[X.pos + 1]++; // 1초후
-			}
+			
 
 		}
 	}
