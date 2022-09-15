79673d31653718f36284f2cc2d33f05ad9f8c53f
jihyuno0o <79901413+jihyuno0o@users.noreply.github.com>
2022-09-15 15:07:03 +0900

    [Silver I] Title: 카드 구매하기, Time: 196 ms, Memory: 17292 KB -BaekjoonHub

--- 백준/Silver/11052. 카드 구매하기/카드 구매하기.java
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 
+// 카드 구매하기
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
@@ -12,16 +13,11 @@ public class Main {
 		int[] dp = new int[N + 1]; // N개를 고르는 가장 비싼 가격 저장
 		dp[1] = card[1]; // 카드 한장을 고르는 가장 비싼 가격은 card[1]을 고르는 것
 
-		int tempMax = 0; //
 		for (int i = 2; i < N + 1; i++) {
+			int tempMax = 0;
 			for (int a = 1; a < i; a++) {
-				for (int b = a; b < i; b++) {
-					if (a + b == i) { // 합해서 i가 되는 a,b의 디피값을 더한다
-						tempMax = Math.max(tempMax, dp[a] + dp[b]);
-					}
-				}
+					tempMax = Math.max(tempMax, dp[a] + dp[i-a]);
 			}
-
 			dp[i] = Math.max(tempMax, card[i]); // dp[i]는 여러개를 합해서 사는게 비싼지, N개의 카드팩을 사는게 비싼지
 		}
 
