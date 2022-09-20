1251bc0b26c6dec5ab14dc897cfe3acc7ae8c5c2
jihyuno0o <79901413+jihyuno0o@users.noreply.github.com>
2022-09-20 11:49:33 +0900

    [Silver III] Title: N과 M (2), Time: 128 ms, Memory: 12936 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,39 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+// N과 M (2)
+public class Main {
+	static int[] num;
+	static int[] sel;
+	static int N, M;
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		M = sc.nextInt();
+		num = new int[N];
+		for(int i=0; i<N; i++) {
+			num[i] = i+1;
+		}
+		sel = new int[M]; // 임시로 저장할 배열
+		
+		Combination(0, 0);
+	}
+	
+	// idx : 해당 위치 숫자 뽑을지 말지
+	// sidx : 뽑은 숫자의 위치
+	public static void Combination(int idx, int sidx) {
+		if(sidx == M) {
+			for(int n : sel) {
+				System.out.print(n);
+				System.out.print(' ');
+			}
+			System.out.println();
+			return;
+		}
+		else if(idx == N) return;
+		sel[sidx] = num[idx]; // 일단 넣고
+		Combination(idx+1, sidx+1); // idx인덱스 숫자 선택
+		Combination(idx+1, sidx); // idx인덱스 숫자 선택 x
+		
+	}
+}
