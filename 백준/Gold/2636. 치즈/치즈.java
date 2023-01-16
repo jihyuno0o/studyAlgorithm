1d44c0d4448b1980b4246d77e8596d28eaa4504a
jihyuno0o <79901413+jihyuno0o@users.noreply.github.com>
2023-01-16 20:51:26 +0900

    [Gold IV] Title: 치즈, Time: 232 ms, Memory: 20792 KB -BaekjoonHub

--- 백준/Gold/2636. 치즈/치즈.java
+++ 백준/Gold/2636. 치즈/치즈.java
@@ -8,7 +8,6 @@ public class Main {
 	static int w;
 	static int[][] cheese; // 입력 저장
 	static boolean[][] air; // 공기 확인
-	static boolean[][] visit; // bfs 방문처리
 	static int remainCheese = 0; // 남아있는 치즈 개수
 	static int[][] dir = { { 1, 0 }, { -1, 0 }, { 0, -1 }, { 0, 1 } };
 
@@ -17,7 +16,6 @@ public class Main {
 		h = sc.nextInt();
 		w = sc.nextInt();
 		cheese = new int[h][w];
-		air = new boolean[h][w];
 
 		for (int i = 0; i < h; i++) {
 			for (int j = 0; j < w; j++) {
@@ -28,10 +26,12 @@ public class Main {
 			}
 		} // 입력
 
-		List<Integer> remain = new LinkedList<>();
-		remain.add(remainCheese);
-		while (remainCheese > 0) {
-			visit = new boolean[h][w];
+		int time = 0;
+		int before = 0;
+
+		while (remainCheese != 0) {
+			before = remainCheese;
+			air = new boolean[h][w];
 			checkAirArea(); // 공기 체크 (구멍빼고 공기만)
 			for (int i = 1; i < h - 1; i++) {
 				for (int j = 1; j < w - 1; j++) {
@@ -46,22 +46,17 @@ public class Main {
 					}
 				}
 			}
-			remain.add(remainCheese); // 남은치즈개수 넣음
+			time++;
 		}
 
-		System.out.println(remain.size() - 1);
-		if (remain.size() == 2) { // 처음부터 한번에 지워지는 경우
-			System.out.println(remain.get(0)); 
-		} else { // 아닌경우
-			System.out.println(remain.get(remain.size() - 2));
-		}
+		System.out.println(time);
+		System.out.println(before);
 
 	}
 
 	public static void checkAirArea() {
 		Queue<int[]> que = new LinkedList<>();
 		air[0][0] = true; // 0,0 은 무조건 공기니까
-		visit[0][0] = true;// 방문처리
 		que.offer(new int[] { 0, 0 });
 
 		while (!que.isEmpty()) {
@@ -69,9 +64,8 @@ public class Main {
 			for (int i = 0; i < 4; i++) {
 				int newR = cur[0] + dir[i][0];
 				int newC = cur[1] + dir[i][1];
-				if (mapOK(newR, newC) && !visit[newR][newC] && cheese[newR][newC] == 0) {
+				if (mapOK(newR, newC) && !air[newR][newC] && cheese[newR][newC] == 0) {
 					air[newR][newC] = true;
-					visit[newR][newC] = true;// 방문처리
 					que.offer(new int[] { newR, newC });
 				}
 			}
