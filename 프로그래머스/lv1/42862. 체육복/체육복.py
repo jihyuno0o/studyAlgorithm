313a9f74ab42424b1e70f0665565748d77b85bdb
jihyuno0o <79901413+jihyuno0o@users.noreply.github.com>
2023-02-22 16:47:02 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42862. 체육복/체육복.py
@@ -0,0 +1,11 @@
+def solution(n, lost, reserve):
+    _reserve = list(set(reserve)-set(lost))
+    _lost = list(set(lost)-set(reserve))
+    #_reserve = [r for r in reserve if r not in lost]
+    #_lost = [l for l in lost if l not in reserve]
+    for i in _reserve:
+        if i-1 in _lost:
+            _lost.remove(i-1)
+        elif i+1 in _lost:
+            _lost.remove(i+1)
+    return n-len(_lost)
